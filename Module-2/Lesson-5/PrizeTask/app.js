a6eeaba442d3139f96ab04b7ade3d4871327133b
muradorucov <orucowmurad@gmail.com>
2022-03-23 13:36:33 +0400

    Module-2 Lesson-5 done

--- Module-2/Lesson-5/PrizeTask/app.js
+++ Module-2/Lesson-5/PrizeTask/app.js
@@ -1,12 +1,7 @@
 let year = prompt('Ili daxil edin !');
 yearNumber= Number(year);
 
-// if(yearNumber%100==0 && yearNumber%400==0){
-//     console.log('İl uzun il deyil')
-// }else 
-//Davam edecem!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-if(yearNumber%4==0){
+if(yearNumber%4==0  && yearNumber%100==0 && yearNumber%400!=0){
     console.log('İl uzun ildi')
 }else{
     console.log('İl uzun il deyil')
