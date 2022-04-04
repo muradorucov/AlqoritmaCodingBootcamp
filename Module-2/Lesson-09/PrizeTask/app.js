aca3fb4602aeac54a981d97c218db69238efb1b1
muradorucov <orucowmurad@gmail.com>
2022-04-04 17:30:49 +0400

    Module-2 Lesson-9 Update

--- Module-2/Lesson-09/PrizeTask/app.js
+++ Module-2/Lesson-09/PrizeTask/app.js
@@ -2,3 +2,15 @@ function numCounter(text) {
     return text.length;
 }
 console.log(numCounter('parametr'));
+
+
+
+/*function numCounter(text){
+    console.log(text.length) ;
+}
+numCounter('sdgjfef');*/
+
+/*function numCounter(){
+    let text =prompt('Enter text');
+    console.log(text.length);
+}*/
