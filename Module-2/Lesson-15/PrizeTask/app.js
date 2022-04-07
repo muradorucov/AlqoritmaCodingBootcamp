ded13b6e12bdb6461865f90f9c07632c0da2f465
muradorucov <orucowmurad@gmail.com>
2022-04-08 00:16:06 +0400

    Module-2 Lesson-15 and Lesson-19 Update

--- Module-2/Lesson-15/PrizeTask/app.js
+++ Module-2/Lesson-15/PrizeTask/app.js
@@ -1,5 +1,11 @@
 function kebabToUpper(text) {
-    let textToUpper = text.toUpperCase();
+    let splitText = text.split('');
+    for (let i = 0; i < splitText.length; i++) {
+        if (splitText[i] === '-') {
+            splitText[i] = '_';
+        }
+    }
+    let textToUpper = splitText.join('').toUpperCase();
     console.log(textToUpper);
 };
 kebabToUpper('i-am-listening-to-fm-while-loading-different-url-on-my-browser-and-also-editing-some-xml-and-html');
--- Module-2/Lesson-19/Task2/app.js
+++ Module-2/Lesson-19/Task2/app.js
@@ -1,21 +1,47 @@
-const cilent = {
+let debt;
+const client = {
     fullName: 'Murad Orucov',
-    creditLimit: 1500,
-    balans: 5000,
-    precentOfMinPayment: 15,
-    getBalance() {
-
-    },
-    getMinPaymant() {
-
-    },
-    withdraw(dleteBalans) {
-        this.balans = this.balans - dleteBalans;
-    },
+    creditLimit: 500,
+    balans: 1000,
+    precentOfMinPayment: 10,
     refill(addToBalans) {
         this.balans = this.balans + addToBalans;
+    },
+    withdraw(deleteBalans) {
+        if (this.balans >= deleteBalans) {
+            this.balans = this.balans - deleteBalans;
+        } else if (this.balans < deleteBalans) {
+            if (deleteBalans <= this.balans + this.creditLimit) {
+                lend = (this.balans + this.creditLimit) - deleteBalans;
+                this.balans = 0 - (this.creditLimit - lend);
+
+                console.log(`Kreditdən istifadə olununan pul ${this.creditLimit - lend}, Qalan kredit ${this.creditLimit -(this.creditLimit - lend) }`);
+            } else {
+                console.log(`Bu məbləği çəkmək mümkün deyil. Kreditle birge ${this.balans + this.creditLimit} manat pul çəkə bilərsiniz.`);
+            }
+        }
+
+    },
+    getBalance() {
+        if (this.balans > 0) {
+            console.log(`Balansınız ${this.balans}`);
+        } else if (this.balans < 0) {
+            console.log(`Borcunuz ${this.balans}`);
+        } else {
+            console.log(`Balans 0-a bərabərdir.`);
+        }
+    },
+    getMinPaymant() {
+        if (this.balans < 0) {
+            debt = this.balans + this.balans * this.precentOfMinPayment
+            console.log("Minimum ödənişiniz " + debt)
+        } else if (this.balans > 0) {
+            console.log("Sizin borcunuz yoxdur");
+        }
     }
 }
-cilent.withdraw(1500);
-cilent.refill(1);
-console.log(cilent)
+
+client.refill(2000);
+client.withdraw(3059);
+client.getBalance();
+client.getMinPaymant();
