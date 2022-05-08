662fcf9366893de1ba3407e838742ab20e01d12f
muradorucov <orucowmurad@gmail.com>
2022-05-08 07:08:06 +0400

    Module-1 Lesson-1 Task1 done

---
+++ Module-4/Lesson-1/Task1/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+
+
+
+const element = (
+    <>
+        <h1>My name is Orujov Murad</h1>
+        <p>This is My First React App</p>
+    </>
+)
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(element,rootElement)
