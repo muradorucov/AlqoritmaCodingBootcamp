eda8fde9d2f7d41e8ee2e6ae3ea8a61e4383fabe
muradorucov <orucowmurad@gmail.com>
2022-05-08 07:11:59 +0400

    Module-4 Lesson-1 Task2 done

---
+++ Module-4/Lesson-1/Task2/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+const language = "JavaScript";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <p>{language} is awesome!</p>
+);
+
