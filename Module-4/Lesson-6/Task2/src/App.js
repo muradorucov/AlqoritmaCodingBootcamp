ae28806b66e47e2b91335bc20d91a49420891f79
muradorucov <orucowmurad@gmail.com>
2022-05-17 14:17:23 +0400

    Module-4 Lesson-6 Task2 done

--- Module-4/Lesson-6/Task2/src/App.js
+++ Module-4/Lesson-6/Task2/src/App.js
@@ -1,18 +1,62 @@
 import React from "react";
 
-export default class App extends React.Component{
-  state={
-    lastKey: null
-  };
-  onKeyPress =(e)=>{
-    this.setState({lastKey:e.key});
-  };
-  componentDidMount(){
-    window.addEventListener("keypress",this.onKeyPress);
+export default class SignupForm extends React.PureComponent{
+  state ={
+    nameValue:"",
+    emailValue:"",
+    passwordValue:""
+  }
+  nameHandleChange=(event)=>{
+    this.setState({
+      nameValue: event.target.value
+    })
+    console.log(this.state.nameValue)
+  }
+  emailHandleChange=(event)=>{
+    this.setState({
+      emailValue: event.target.value
+    })
+    console.log(this.state.emailValue)
+  }
+  passwordHandleChange=(event)=>{
+    this.setState({
+      passwordValue: event.target.value
+    })
+    console.log(this.state.passwordValue)
+  }
+  onClickChange=()=>{
+    this.setState({nameValue:"Murad",emailValue:"oricowmurad@gmail.com",passwordValue:"RWMa5Qyf"})
+  }
+  onClickDeleteName=()=>{
+    this.setState({nameValue:""})
+  }
+  onClickDeleteEmail=()=>{
+    this.setState({emailValue:""})
+  }
+  onClickDeletePassword=()=>{
+    this.setState({passwordValue:""})
   }
   render(){
+    let {nameValue,emailValue,passwordValue}=this.state
     return(
-      <main>lastKey: {this.state.lastKey}</main>
+      <div>
+        <label htmlFor="name">
+          Name: <input id="name" onChange={this.nameHandleChange} value={nameValue} />
+        </label>
+        <button onClick={this.onClickDeleteName}>x</button>
+        <br></br>
+        <label htmlFor="email">
+          Email: <input id="email" onChange={this.emailHandleChange} value={emailValue}/>
+        </label>
+        <button onClick={this.onClickDeleteEmail}>x</button>
+        <br></br>
+        <label htmlFor="password">
+          Password: <input id="password" onChange={this.passwordHandleChange} value={passwordValue}/>
+        </label>
+        <button onClick={this.onClickDeletePassword}>x</button>
+        <br></br>
+        <button onClick={this.onClickChange}>Change Default Value</button>
+      </div>
     )
   }
 }
