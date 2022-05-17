758d179c11b3d5f75b53908bb8852e8f7358dc8f
muradorucov <orucowmurad@gmail.com>
2022-05-17 14:50:15 +0400

    Module-4 Lesson-6 Task3 done

--- Module-4/Lesson-6/Task3/src/App.js
+++ Module-4/Lesson-6/Task3/src/App.js
@@ -1,29 +1,66 @@
 import React from "react";
 
-export default class App extends React.Component{
-  state={
-    lastKey: null,
-    className:'App'
-  };
-  onKeyPress =(e)=>{
-    this.setState({lastKey:e.key});
-  };
-  componentDidMount(){
-    window.addEventListener("keypress",this.onKeyPress);
+export default class SignupForm extends React.PureComponent{
+  state ={
+    nameValue:"",
+    emailValue:"",
+    passwordValue:"",
+    isActive:false,
+  }
+  nameHandleChange=(event)=>{
+    this.setState({
+      nameValue: event.target.value
+    })
+  }
+  emailHandleChange=(event)=>{
+    this.setState({
+      emailValue: event.target.value
+    })
+  }
+  passwordHandleChange=(event)=>{
+    this.setState({
+      passwordValue: event.target.value
+    })
+  }
+  handleChangeChackbox=()=>{
+    this.setState({isActive:true})
+  }
+  onClickChange=()=>{
+    this.setState({nameValue:"Murad",emailValue:"oricowmurad@gmail.com",passwordValue:"RWMa5Qyf"})
+  }
+  onClickDeleteName=()=>{
+    this.setState({nameValue:""})
+  }
+  onClickDeleteEmail=()=>{
+    this.setState({emailValue:""})
+  }
+  onClickDeletePassword=()=>{
+    this.setState({passwordValue:""})
   }
   render(){
-    let {lastKey,className} =this.state;
-    switch (lastKey) {
-      case '1':
-        className="App App-red";
-        break;
-      case '2':
-        className="App App-blue";
-        break;
-    }
+    let {nameValue,emailValue,passwordValue,isActive}=this.state
     return(
-      <div className={className}>
-        <p>lastKey: {this.state.lastKey}</p>
+      <div>
+        <label htmlFor="name">
+          Name: <input id="name" onChange={this.nameHandleChange} value={nameValue} />
+        </label>
+        <button onClick={this.onClickDeleteName}>x</button>
+        <br></br>
+        <label htmlFor="email">
+          Email: <input id="email" onChange={this.emailHandleChange} value={emailValue}/>
+        </label>
+        <button onClick={this.onClickDeleteEmail}>x</button>
+        <br></br>
+        <label htmlFor="password">
+          Password: <input type={isActive ? "text": "password"}  id="password" onChange={this.passwordHandleChange} value={passwordValue}/>
+        </label>
+        <button onClick={this.onClickDeletePassword}>x</button>
+        <br></br>
+        <label htmlFor="show">
+          Show Password: <input type="checkbox" checked={isActive} onChange={this.handleChangeChackbox} id="show"/>
+        </label>
+        <br></br>
+        <button onClick={this.onClickChange}>Change Default Value</button>
       </div>
     )
   }
